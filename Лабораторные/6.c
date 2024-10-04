274e9742fdc7f8aa37f96a169847ca31d23d03e1
LibTower <119515731+LibTower@users.noreply.github.com>
2024-10-04 23:37:12 +0700

    Update 6.c

--- Лабораторные/6.c
+++ Лабораторные/6.c
@@ -1,3 +1,6 @@
+/*
+	Программа перебирает масив, и если находит в нем пару чисел идущих подряд и удаляет ее. 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,13 +10,16 @@ int main() {
 	int j;
 	int mass[N];
 	srand(time(NULL));
+	
 	for(int i = 0; i<N; i++){
 		mass[i] = rand()%10;
 	}
+	
 	for(int i = 0; i<N; i++){
 		printf("%d ", mass[i]);
 	}
 	printf("\n");
+	
 	for(int i = 1; i<N; i++){
 		if(mass[i]-1 == mass[i-1] && count == 0) {
 			count+=1;
@@ -23,11 +29,13 @@ int main() {
 			count+=1;
 		}
 	}
+	
 	printf("Массив без первой пары: ");
 	for(int i = 0; i<N; i++){
 		if(i != j && j != i-1){
 		printf("%d ", mass[i]);
 		}
 	}
+	
 	printf("\nКоличество пар: %d\n", count);
 }
